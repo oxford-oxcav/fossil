ebb1ca491cc99d9d465af7a9dafbb4dc9a74c5de
Alec <alec.edwards@worc.ox.ac.uk>
2022-10-13 16:36:46 +0100

    Fixed Marabou imports to be optional (though error messages still print)

--- src/verifier.py
+++ src/verifier.py
@@ -17,12 +17,13 @@ from aenum import Enum, NoAlias
 
 try:
     import dreal as dr
-except:
+except ModuleNotFoundError:
     logging.exception("No dreal")
 try:
     from maraboupy import Marabou
-except ModuleNotFoundError:
+except ImportError as e:
     logging.exception("Exception while importing Marabou")
+    Marabou = None
 
 
 from src.shared.cegis_values import CegisConfig, CegisStateKeys
@@ -392,94 +393,95 @@ class VerifierZ3(Verifier):
         super().__init__(n_vars, constraints_method, vars_bounds, z3_vars, **kw)
 
 
-class VerifierMarabou(Verifier):
-    def __init__(self, n_vars, constraints_method, vars_bounds, solver_vars, **kw):
-        self.inner = kw.get(CegisConfig.INNER_RADIUS.k, CegisConfig.INNER_RADIUS.v)
-        self.outer = kw.get(CegisConfig.OUTER_RADIUS.k, CegisConfig.OUTER_RADIUS.v)
-        super().__init__(n_vars, constraints_method, vars_bounds, solver_vars, **kw)
+if Marabou:
+    class VerifierMarabou(Verifier):
+        def __init__(self, n_vars, constraints_method, vars_bounds, solver_vars, **kw):
+            self.inner = kw.get(CegisConfig.INNER_RADIUS.k, CegisConfig.INNER_RADIUS.v)
+            self.outer = kw.get(CegisConfig.OUTER_RADIUS.k, CegisConfig.OUTER_RADIUS.v)
+            super().__init__(n_vars, constraints_method, vars_bounds, solver_vars, **kw)
 
-    @staticmethod
-    def new_vars(n):
-        return range(n)
+        @staticmethod
+        def new_vars(n):
+            return range(n)
 
-    def new_solver(self):
-        return Marabou.createOptions(verbosity=0)
+        def new_solver(self):
+            return Marabou.createOptions(verbosity=0)
 
-    @staticmethod
-    def solver_fncts() -> Dict[str, Callable]:
-        return {"And": None, "Or": None, "Estim_Activation": np.max}
+        @staticmethod
+        def solver_fncts() -> Dict[str, Callable]:
+            return {"And": None, "Or": None, "Estim_Activation": np.max}
 
-    def is_sat(self, res: Dict) -> bool:
-        return bool(res)
+        def is_sat(self, res: Dict) -> bool:
+            return bool(res)
 
-    def is_unsat(self, res: Dict) -> bool:
-        return not bool(res)
+        def is_unsat(self, res: Dict) -> bool:
+            return not bool(res)
 
-    def _solver_solve(self, solver, fml):
-        results = [
-            quadrant.solve(options=solver, verbose=False)[0] for quadrant in fml
-        ]  # List of dicts of counter examples for each quadrant
-        combined_results = {}
-        for result in results:
-            combined_results.update(
-                result
-            )  # This means counterexamples are lost - not a permanent solution
-        return combined_results
+        def _solver_solve(self, solver, fml):
+            results = [
+                quadrant.solve(options=solver, verbose=False)[0] for quadrant in fml
+            ]  # List of dicts of counter examples for each quadrant
+            combined_results = {}
+            for result in results:
+                combined_results.update(
+                    result
+                )  # This means counterexamples are lost - not a permanent solution
+            return combined_results
 
-    def _solver_model(self, solver, res):
-        return res
+        def _solver_model(self, solver, res):
+            return res
 
-    def _model_result(self, solver, model, x, i):
-        return model[x]
+        def _model_result(self, solver, model, x, i):
+            return model[x]
 
-    def domain_constraints(
-        self, V: Marabou.MarabouNetworkONNX, Vdot: Marabou.MarabouNetworkONNX
-    ):
-        """
-        :param V:
-        :param Vdot:
-        :return: tuple of Marabou ONNX networks with appropriately set input/ output bounds
-        """
-        inner = self.inner
-        outer = self.outer
+        def domain_constraints(
+            self, V: Marabou.MarabouNetworkONNX, Vdot: Marabou.MarabouNetworkONNX
+        ):
+            """
+            :param V:
+            :param Vdot:
+            :return: tuple of Marabou ONNX networks with appropriately set input/ output bounds
+            """
+            inner = self.inner
+            outer = self.outer
 
-        orthants = list(product(*[[1.0, -1.0] for i in range(self.n)]))
-        V_input_vars = V.inputVars[0][0]
-        Vdot_input_vars = Vdot.inputVars[0][0]
-        V_output_vars = V.outputVars[0]
-        Vdot_output_vars = Vdot.outputVars[0]
+            orthants = list(product(*[[1.0, -1.0] for i in range(self.n)]))
+            V_input_vars = V.inputVars[0][0]
+            Vdot_input_vars = Vdot.inputVars[0][0]
+            V_output_vars = V.outputVars[0]
+            Vdot_output_vars = Vdot.outputVars[0]
 
-        V_tuple = tuple(deepcopy(V) for i in range(len(orthants)))
-        Vdot_tuple = tuple(
-            deepcopy(Vdot) for i in range(len(orthants))
-        )  # Definitely bad usage of memory - a generator would be better
-        assert (V_input_vars == Vdot_input_vars).all()
+            V_tuple = tuple(deepcopy(V) for i in range(len(orthants)))
+            Vdot_tuple = tuple(
+                deepcopy(Vdot) for i in range(len(orthants))
+            )  # Definitely bad usage of memory - a generator would be better
+            assert (V_input_vars == Vdot_input_vars).all()
 
-        # TODO: This now covers all 2^n orthants, but excludes a small 'cross' region around the axis - I think this is why
-        # your approach is to add regions that overlap. Will add this but not urgent just yet (seems to work for very low inner cords)
-        for i, orthant in enumerate(orthants):
+            # TODO: This now covers all 2^n orthants, but excludes a small 'cross' region around the axis - I think this is why
+            # your approach is to add regions that overlap. Will add this but not urgent just yet (seems to work for very low inner cords)
+            for i, orthant in enumerate(orthants):
 
-            for j, var in enumerate(V_input_vars):
-                V_tuple[i].setLowerBound(
-                    var, min(orthant[j] * inner, orthant[j] * outer)
-                )
-                V_tuple[i].setUpperBound(
-                    var, max(orthant[j] * inner, orthant[j] * outer)
-                )
-                Vdot_tuple[i].setLowerBound(
-                    var, min(orthant[j] * inner, orthant[j] * outer)
-                )
-                Vdot_tuple[i].setUpperBound(
-                    var, max(orthant[j] * inner, orthant[j] * outer)
-                )
+                for j, var in enumerate(V_input_vars):
+                    V_tuple[i].setLowerBound(
+                        var, min(orthant[j] * inner, orthant[j] * outer)
+                    )
+                    V_tuple[i].setUpperBound(
+                        var, max(orthant[j] * inner, orthant[j] * outer)
+                    )
+                    Vdot_tuple[i].setLowerBound(
+                        var, min(orthant[j] * inner, orthant[j] * outer)
+                    )
+                    Vdot_tuple[i].setUpperBound(
+                        var, max(orthant[j] * inner, orthant[j] * outer)
+                    )
 
-            V_tuple[i].setUpperBound(V_output_vars[0], 0)
-            Vdot_tuple[i].setLowerBound(Vdot_output_vars[0], 0)
+                V_tuple[i].setUpperBound(V_output_vars[0], 0)
+                Vdot_tuple[i].setLowerBound(Vdot_output_vars[0], 0)
 
-        pass
+            pass
 
-        for cs in ({"lyap": (*V_tuple, *Vdot_tuple)},):
-            yield cs
+            for cs in ({"lyap": (*V_tuple, *Vdot_tuple)},):
+                yield cs
 
 def get_verifier_type(verifier: Literal) -> Verifier:
     if verifier == VerifierType.DREAL:
